42ec4cf7f627e72c0963c7dca29d578531c1d7e6
Horacio Alexandro <horacio@swoopin.net>
2022-08-19 20:51:16 -0700

    Initial commit

---
+++ Sources/iOSMath.xcframework/ios-arm64_x86_64-simulator/iOSMath.framework/Headers/iOSMath.h
@@ -0,0 +1,18 @@
+//
+//  iOSMath.h
+//  iOSMath
+//
+//  Created by Horacio Alexandro Sanchez on 8/19/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSMath.
+FOUNDATION_EXPORT double iOSMathVersionNumber;
+
+//! Project version string for iOSMath.
+FOUNDATION_EXPORT const unsigned char iOSMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSMath/PublicHeader.h>
+
+
