827dc83d438f02551334ae94dae2f23d9d9c0c9a
Horacio Alexandro <horacio@swoopin.net>
2022-08-19 22:01:41 -0700

    Changed headers!!

--- Sources/iOSMath.xcframework/ios-arm64_x86_64-maccatalyst/iOSMath.framework/Versions/A/Headers/iOSMath.h
+++ Sources/iOSMath.xcframework/ios-arm64_x86_64-maccatalyst/iOSMath.framework/Versions/A/Headers/iOSMath.h
@@ -6,10 +6,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <iOSMath/IosMath.h>
 #import <IosMath/MTMathUILabel.h>
 #import <IosMath/MTMathListDisplay.h>
 #import <IosMath/MTMathList.h>
 #import <IosMath/MTMathListBuilder.h>
+
 //! Project version number for iOSMath.
 FOUNDATION_EXPORT double iOSMathVersionNumber;
 
